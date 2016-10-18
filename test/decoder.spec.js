86f8ce7e5edfaf401845124744317a37f0d66ace
script 	panic('ghost in the shell')/script @ziyasal <sarikayaziya@gmail.com>
2016-10-18 15:14:35 +0200

    Update decoder.spec.js

--- test/decoder.spec.js
+++ test/decoder.spec.js
@@ -2,9 +2,9 @@ import test from 'ava';
 import decode from '../lib/decode';
 
 test('should decode basic', t => {
-  let pairs = [{Key: "foo/bar", Value: "bazqqqqq"}];
+  let pairs = [{Key: "foo/bar", Value: "baz"}];
 
-  let expected = {bar: "bazqqqqq"};
+  let expected = {bar: "baz"};
 
   let actual = decode(pairs, 'foo/');
 
@@ -13,11 +13,11 @@ test('should decode basic', t => {
 
 test('should decode nested', t => {
   let pairs = [
-    {Key: "foo/bar", Value: "bazqqqqq"},
-    {Key: "foo/child/data", Value: "UOOOOOO"}
+    {Key: "foo/bar", Value: "baz"},
+    {Key: "foo/child/data", Value: "ghost"}
   ];
 
-  let expected = {bar: "bazqqqqq", child: {data: "UOOOOOO"}};
+  let expected = {bar: "baz", child: {data: "ghost"}};
 
   let actual = decode(pairs, 'foo/');
 
